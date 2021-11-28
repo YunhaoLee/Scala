9d391a8a70cda01d19c1bf256c867727141bd5ed
Lee <17806286945@163.com>
2021-11-28 16:46:33 +0800

    second commit

--- src/main/scala/chapter01/HelloWorld.scala
+++ src/main/scala/chapter01/HelloWorld.scala
@@ -1,7 +1,16 @@
 package chapter01
 
+/*
+  Object:关键字，声明一个单例对象（伴生对象）
+ */
 object HelloWorld {
+
+  /*
+    main方法：从外部可直接调用执行的方法
+    def 方法名称（参数名称: 参数类型）: 返回值类型 = { 方法体 }
+   */
   def main(args: Array[String]): Unit = {
     println("hello world")
+    System.out.println("hello java")
   }
 }
